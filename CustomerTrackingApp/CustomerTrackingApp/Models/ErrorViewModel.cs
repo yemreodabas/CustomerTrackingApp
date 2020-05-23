76c95667c21a28b99a8f69afbabab808eda7cec8
yemreodabas <yemredbs@gmail.com>
2020-05-23 17:30:29 +0300

    project created

---
+++ CustomerTrackingApp/CustomerTrackingApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CustomerTrackingApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
